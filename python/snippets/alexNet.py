eb99864e0e65c9e031f1a7e240920691969472c3
shadySource <alec0061@hotmail.com>
2017-02-07 14:24:03 -0500

    update new model

--- python/snippets/alexNet.py
+++ python/snippets/alexNet.py
@@ -1,6 +1,7 @@
 # The Model of DeepVO
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.advanced_activations import PReLU
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
 
@@ -11,54 +12,54 @@ def create_model():
     model = Sequential()
     model.add(Convolution2D(96, 11, 11, border_mode='same', input_shape=(256, 256, 6)))
     model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(4, 4)))
-    
+    model.add(PReLU())
+    model.add(MaxPooling2D(pool_size=(3, 3)))
+
     model.add(Convolution2D(256, 5, 5, border_mode='same'))
     model.add(BatchNormalization())
-    model.add(Activation('relu'))
+    model.add(PReLU())
     model.add(MaxPooling2D(pool_size=(3, 3)))
-    
+
     model.add(Convolution2D(384, 3, 3, border_mode='same'))
     model.add(BatchNormalization())
-    model.add(Activation('relu'))
+    model.add(PReLU())
     model.add(MaxPooling2D(pool_size=(3, 3)))
-    
+
     model.add(Convolution2D(384, 3, 3, border_mode='same'))
     model.add(BatchNormalization())
-    model.add(Activation('relu'))
+    model.add(PReLU())
     model.add(MaxPooling2D(pool_size=(3, 3)))
-    
+
     model.add(Flatten())
     model.add(Dense(4096, init='normal'))
     model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    
+    model.add(PReLU())
+
     model.add(Dense(4096, init='normal'))
     model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    
+    model.add(PReLU())
+
     model.add(Dense(4096, init='normal'))
     model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    
+    model.add(PReLU())
+
     model.add(Dense(1024, init='normal'))
     model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    
+    model.add(PReLU())
+
     model.add(Dense(3, init='normal'))
     model.add(BatchNormalization())
-    model.add(Activation('softmax'))
+    model.add(Activation('linear'))
 
     return model
 
 def run_model(model, Xtr, Ytr, Xte, Yte):
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
-    
-    model.fit(Xtr, Ytr, batch_size=8, nb_epoch=60, verbose=1)
-    
+
+    model.fit(Xtr, Ytr, batch_size=8, nb_epoch=1, verbose=1)
+
     score = model.evaluate(Xte, Yte, verbose=1)
-    
+    model.save("/home/sexy/source/deep-visual-odometry/models/alecNet_1/alecNet_1_trined.h5")
     return score
 
 
@@ -73,4 +74,3 @@ def run_model(model, Xtr, Ytr, Xte, Yte):
 
 
 
-    
--- python/snippets/main.py
+++ python/snippets/main.py
@@ -2,5 +2,5 @@
 import formatData
 import alexNet
 
-Xtr, Ytr, Xte, Yte = formatData.knownEnv(formatData.load_data(),formatData.load_poses())
+Xtr, Ytr, Xte, Yte = formatData.knownEnv(formatData.load_data(),formatData.load_poses(), training_ratio=(7/10.0))
 alexNet.run_model(alexNet.create_model(), Xtr, Ytr, Xte, Yte)
